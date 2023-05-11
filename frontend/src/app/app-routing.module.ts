852ce0cfa7f67d213931c21918f016c8e43177c4
Yoshi <yoshi@komplexworld.com>
2023-05-11 21:52:26 +0200

    included footer
    Co-authored-by: Jannik Springer <Jacky-Ickx@users.noreply.github.com>

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ImprintComponent } from './pages/imprint/imprint.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'imprint',
+    component: ImprintComponent
   },
   {
     path: '404',
