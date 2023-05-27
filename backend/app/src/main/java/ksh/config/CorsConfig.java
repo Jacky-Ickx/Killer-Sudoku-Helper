d48baaf79a52fd74a77aeb27fa517fa7ac9c598a
Jannik Springer <jannik@jannikspringer.dev>
2023-05-27 23:13:10 +0200

    added Websocket endpoint/controller

--- backend/app/src/main/java/ksh/config/CorsConfig.java
+++ backend/app/src/main/java/ksh/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package ksh;
+package ksh.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class Config implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
