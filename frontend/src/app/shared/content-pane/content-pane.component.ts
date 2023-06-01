fbe58dd5be6ac7eb0c9669929df41431e0f4f5e0
Jannik Springer <jannik@jannikspringer.dev>
2023-06-01 17:47:38 +0200

    added centering option to content pane
    if false, content pane will justify content to top
    if true, content pane will justify content to center

--- frontend/src/app/shared/content-pane/content-pane.component.ts
+++ frontend/src/app/shared/content-pane/content-pane.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content-pane',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-pane.component.scss']
 })
 export class ContentPaneComponent {
-
+    @Input() centered: boolean = false;
 }
